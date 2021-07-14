2c99fa394d142bbf603713b28158a71b33ff38f5
Felipe Suknaic <fsuknaic@hotmail.com>
2021-07-14 15:20:05 -0300

    middleware para capturas de erros

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Response, Request } from 'express';
+import 'express-async-errors';
 import swaggerUI from 'swagger-ui-express';
 
+import { AppError } from './error/AppError';
 import { routes } from './routes';
 import swaggerFile from './swagger.json';
 
@@ -14,5 +16,19 @@ app.use(express.json());
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'internal server error',
+  });
+});
 
 app.listen(3333, () => console.log('server is running'));
