c5e4e9d42de364bcd265a2054ca77c7448dfec69
Felipe Suknaic <fsuknaic@hotmail.com>
2021-07-14 18:13:50 -0300

    delete de olders avatars

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
+import { deleteFile } from '../../../../utils/file';
 import { IUSerRepository } from '../../repositories/IUserRepository';
 
 interface IRequest {
@@ -16,6 +17,10 @@ class UpdateUseAvatarUseCase {
 
   async execulte({ user_id, avatar_file }: IRequest): Promise<void> {
     const user = await this.userRepository.findById(user_id);
+
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }
 
     user.avatar = avatar_file;
 
