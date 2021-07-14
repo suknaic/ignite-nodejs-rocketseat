9fbf5e2f76218f215a4f9b0fdf950bedbc7f459f
Felipe Suknaic <fsuknaic@hotmail.com>
2021-07-14 14:55:16 -0300

    criando rotas authenticadas

--- src/modules/accounts/repositories/implementations/UserRepository.ts
+++ src/modules/accounts/repositories/implementations/UserRepository.ts
@@ -11,6 +11,12 @@ class UserRepository implements IUSerRepository {
     this.repository = getRepository(User);
   }
 
+  async findById(id: string): Promise<User> {
+    const user = await this.repository.findOne(id);
+
+    return user;
+  }
+
   async findByEmail(email: string): Promise<User> {
     const user = await this.repository.findOne({ email });
 
--- src/routes/specification.routes.ts
+++ src/routes/specification.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { CreateSpecificationController } from '../modules/cars/useCases/createSpecification/CreateSpecificationController';
 
 const createSpecificationController = new CreateSpecificationController();
 
 const specificationRouter = Router();
 
+specificationRouter.use(ensureAuthenticated);
 specificationRouter.post('/', createSpecificationController.handle);
 
 export { specificationRouter };
